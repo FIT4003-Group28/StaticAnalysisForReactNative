8b51455eee56c65c3909ebe8993559d72e3d31c3
Josh Boul <jbou0013@student.monash.edu>
2022-09-07 19:20:13 +1000

    Improved the number of results found by github API to find all

--- get-react-native-repos.py
+++ get-react-native-repos.py
@@ -1,12 +1,17 @@
+from functools import total_ordering
+from multiprocessing.reduction import duplicate
 import requests, json, datetime, time
 from cred import ACCESS_TOKEN
 from progress.bar import Bar
 from datetime import timedelta
 
+
 # Define constants
 TOPIC = "react-native"
+LANGUAGE = ' language:"javascript" language:"typescript"'
+DAYS_PER_ITERATION = 7
+LOWEST_FILTER_DATE = datetime.datetime(2015, 1, 1)
 PER_PAGE = 100
-MAX_RESULTS = 34500
 URL_PREFIX = "https://api.github.com/search/repositories?q="
 AUTH_HEADER = {'Authorization': 'token %s' % ACCESS_TOKEN, 'Accept': 'application/vnd.github.v3.raw'}
 
@@ -15,46 +20,81 @@ AUTH_HEADER = {'Authorization': 'token %s' % ACCESS_TOKEN, 'Accept': 'applicatio
 output = []
 output_dict = {}
 current_filter_date = datetime.datetime.now()
-LOWEST_FILTER_DATE = datetime.datetime(2015, 1, 1)
+
+
+# Get total number of expected results
+def get_total_num_results():
+    # Return new URL    
+    url = URL_PREFIX + "topic:" + TOPIC + LANGUAGE + " pushed:>" + LOWEST_FILTER_DATE.strftime("%Y-%m-%dT%H:%M:%S")
+
+    try:
+        response = requests.get(url, headers=AUTH_HEADER)
+        response_dict = response.json()
+        return response_dict['total_count']
+    except:
+        time.sleep(5)
+
 
 # Get the date range for URL
-def get_date_range():
+def get_date_range(decrement_value):
     global current_filter_date
-    lower_date_range = (current_filter_date - timedelta(days=7)).strftime("%Y-%m-%dT%H:%M:%S")
+
+    lower_date_range = (current_filter_date - timedelta(days=decrement_value)).strftime("%Y-%m-%dT%H:%M:%S")
     return lower_date_range + ".." + current_filter_date.strftime("%Y-%m-%dT%H:%M:%S")
 
+
 # Decrement the date
-def decrement_date():
+def decrement_date(decrement_value):
     global current_filter_date
-    current_filter_date = current_filter_date - timedelta(days=7)
+    current_filter_date = current_filter_date - timedelta(days=decrement_value)
+
 
 # Function to get request URL
-def get_repos(page_num):
-    filter = " pushed:" + get_date_range()
+def get_repos(page_num, decrement_value):
+    filter = " pushed:" + get_date_range(decrement_value) 
 
-    # Return new URL    
-    url = URL_PREFIX + "topic:" + TOPIC + filter + "&page=" + str(page_num) + "&per_page=" + str(PER_PAGE)
+    # Return new URL
+    url = URL_PREFIX + "topic:" + TOPIC + LANGUAGE + filter + "&page=" + str(page_num) + "&per_page=" + str(PER_PAGE)
 
     while True:
         try:
             response = requests.get(url, headers=AUTH_HEADER)
             response_dict = response.json()
+
+            if response_dict['total_count'] > 1000:
+                # print("\n" + str(response_dict['total_count']) + " for date range " + get_date_range())
+                return False
+            
             return response_dict['items']
         except:
             time.sleep(5)
 
+
 # Get repos and append them to file per page loaded
-with Bar('Processing', max=MAX_RESULTS) as bar:
+with Bar('Processing', max=get_total_num_results()) as bar:
     with open('repos.json', 'w', encoding='utf-8') as f:
         current_page = 1
+        decrement_value = DAYS_PER_ITERATION
+        duplicate_hits = 0
+        total_loops = 0
 
         while current_filter_date > LOWEST_FILTER_DATE:
-            repos = get_repos(current_page)
+            repos = get_repos(current_page, decrement_value)
+            
+            # Check that number of results is less than 1000
+            if repos == False:
+                if decrement_value == DAYS_PER_ITERATION:
+                    decrement_value = 1
+                elif decrement_value <= 1:
+                    decrement_value /= 2
+                continue
+
             output = []
 
             # Record resulting clone URLs
             for repo in repos:
                 if repo['full_name'] in output_dict.keys():
+                    duplicate_hits += 1
                     continue
 
                 dictionary = {
@@ -78,12 +118,17 @@ with Bar('Processing', max=MAX_RESULTS) as bar:
             current_page += 1
 
             if current_page > 10 or len(repos) < PER_PAGE:
-                decrement_date()
+                decrement_date(decrement_value)
+                decrement_value = DAYS_PER_ITERATION
                 current_page = 1
 
+                total_loops += 1
+            
         bar.finish()
     f.close()
 
+print("Duplicates hit: " + str(duplicate_hits) + " times")
+print("Total loops: " + str(total_loops) + " times")
 
 # Read in repos from file
 f = open("repos.json", "r")
@@ -94,7 +139,7 @@ f.close()
 for i in range(0, len(unsorted_repos)):
     unsorted_repos[i] = (json.loads(unsorted_repos[i]))
 
-print("\nSorting repos")
+print("\nSorting repos...")
 # Sort array
 sorted_output = sorted(unsorted_repos, key=lambda x: (x['stars'], x['updated_at']), reverse=True)
 del unsorted_repos
@@ -104,3 +149,4 @@ with open('repos.json', 'w', encoding='utf-8') as f:
     for i in range(0, len(sorted_output)):
         f.write(json.dumps(sorted_output[i]) + "\n")
 f.close()
+print("Getting repos complete!\n")

e06094330632de4e9895c2187c32774203ed99ff
Josh Boul <jbou0013@student.monash.edu>
2022-09-07 13:47:20 +1000

    Updating script to get far more repos

--- get-react-native-repos.py
+++ get-react-native-repos.py
@@ -1,92 +1,106 @@
-import requests, json, datetime, operator
+import requests, json, datetime, time
 from cred import ACCESS_TOKEN
 from progress.bar import Bar
+from datetime import timedelta
 
 # Define constants
 TOPIC = "react-native"
-SORT = "stars"
-ORDER = "desc"
 PER_PAGE = 100
-MAX_ITERATIONS = 1000 # <-- important since GitHub has a maximum of 1000 results per query
-MAX_RESULTS = 34000
+MAX_RESULTS = 34500
 URL_PREFIX = "https://api.github.com/search/repositories?q="
 AUTH_HEADER = {'Authorization': 'token %s' % ACCESS_TOKEN, 'Accept': 'application/vnd.github.v3.raw'}
 
-# Check constants
-if MAX_ITERATIONS < PER_PAGE:
-    raise Exception("PER_PAGE value must be less than MAX_ITERATIONS")
-
-NUM_OF_PAGES_PER_REQUEST = int(MAX_ITERATIONS / PER_PAGE) 
 
 # Begin repo list
 output = []
 output_dict = {}
-low_range = 1
-high_range = 1 + NUM_OF_PAGES_PER_REQUEST
-date_now = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
-stars = 999999
-filter = " stars:<" + str(stars) + " pushed:<" + date_now
+current_filter_date = datetime.datetime.now()
+LOWEST_FILTER_DATE = datetime.datetime(2015, 1, 1)
 
+# Get the date range for URL
+def get_date_range():
+    global current_filter_date
+    lower_date_range = (current_filter_date - timedelta(days=7)).strftime("%Y-%m-%dT%H:%M:%S")
+    return lower_date_range + ".." + current_filter_date.strftime("%Y-%m-%dT%H:%M:%S")
+
+# Decrement the date
+def decrement_date():
+    global current_filter_date
+    current_filter_date = current_filter_date - timedelta(days=7)
 
 # Function to get request URL
-def get_url(page_num):
-    global filter, date_now
-    # After 1000 results, get next stars/dates
-    if page_num == 1 and len(output) > 0:
-        filter = " stars:<=" + str((output[-1])['stars']) + " pushed:<" + date_now
+def get_repos(page_num):
+    filter = " pushed:" + get_date_range()
 
     # Return new URL    
-    return URL_PREFIX + "topic:" + TOPIC + filter + "&sort=" + SORT + "&order=" + ORDER + "&page=" + str(page_num) + "&per_page=" + str(PER_PAGE)
+    url = URL_PREFIX + "topic:" + TOPIC + filter + "&page=" + str(page_num) + "&per_page=" + str(PER_PAGE)
 
+    while True:
+        try:
+            response = requests.get(url, headers=AUTH_HEADER)
+            response_dict = response.json()
+            return response_dict['items']
+        except:
+            time.sleep(5)
+
+# Get repos and append them to file per page loaded
 with Bar('Processing', max=MAX_RESULTS) as bar:
-    inf_loop_detection = 0
-    while len(output) < MAX_RESULTS:
-        if inf_loop_detection == len(output) and inf_loop_detection > 0:
-            print("\ninfinite loop detected")
-            break
-        inf_loop_detection = len(output)
+    with open('repos.json', 'w', encoding='utf-8') as f:
+        current_page = 1
 
-        for i in range(low_range, high_range):
-            try:
-                # Send GET request
-                url = get_url(i-(low_range-1))
-                
-                response = requests.get(url, headers=AUTH_HEADER)
-                response_dict = response.json()
-                repos = response_dict['items']
-            except:
-                break
+        while current_filter_date > LOWEST_FILTER_DATE:
+            repos = get_repos(current_page)
+            output = []
 
             # Record resulting clone URLs
-            for j in range(0, len(repos)):
-                if repos[j]['full_name'] in output_dict.keys():
+            for repo in repos:
+                if repo['full_name'] in output_dict.keys():
                     continue
 
                 dictionary = {
-                    'name': repos[j]['full_name'],
-                    'clone_url': repos[j]['clone_url'],
-                    'stars': repos[j]['stargazers_count'],
-                    'forks': repos[j]['forks_count'],
-                    'updated_at': repos[j]['updated_at'],
-                    'default_branch': repos[j]['default_branch']
+                    'name': repo['full_name'],
+                    'clone_url': repo['clone_url'],
+                    'stars': repo['stargazers_count'],
+                    'forks': repo['forks_count'],
+                    'updated_at': repo['updated_at'],
+                    'default_branch': repo['default_branch']
                 }
 
                 # Create a json object and add to array
-                output_dict[repos[j]['full_name']] = len(output)
+                output_dict[repo['full_name']] = len(output)
                 output.append(dictionary)
 
-                bar.next()
+                bar.next()  
+
+            for i in range(0, len(output)):
+                f.write(json.dumps(output[i]) + "\n")  
             
-        # Sort array
-        output = sorted(output, key=lambda x: (x['stars'], x['updated_at']), reverse=True)
+            current_page += 1
 
-        # Iterate variables
-        low_range += NUM_OF_PAGES_PER_REQUEST
-        high_range += NUM_OF_PAGES_PER_REQUEST
+            if current_page > 10 or len(repos) < PER_PAGE:
+                decrement_date()
+                current_page = 1
 
-    bar.finish()
+        bar.finish()
+    f.close()
 
-# Output repo URLs to a text file
+
+# Read in repos from file
+f = open("repos.json", "r")
+unsorted_repos = f.read().splitlines()
+f.close()
+
+# Convert list to list of JSON objects instead of strings
+for i in range(0, len(unsorted_repos)):
+    unsorted_repos[i] = (json.loads(unsorted_repos[i]))
+
+print("\nSorting repos")
+# Sort array
+sorted_output = sorted(unsorted_repos, key=lambda x: (x['stars'], x['updated_at']), reverse=True)
+del unsorted_repos
+
+# Output sorted repos back to text file
 with open('repos.json', 'w', encoding='utf-8') as f:
-    for i in range(0, len(output)):
-        f.write(json.dumps(output[i]) + "\n")
+    for i in range(0, len(sorted_output)):
+        f.write(json.dumps(sorted_output[i]) + "\n")
+f.close()

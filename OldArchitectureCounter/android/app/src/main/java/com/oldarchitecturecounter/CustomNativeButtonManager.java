497da927141049e8d8b8d4af2c4c32056235815c
taha <tosm0001@student.monash.edu>
2022-09-27 20:34:04 +0800

    Renamed counter app

---
+++ OldArchitectureCounter/android/app/src/main/java/com/oldarchitecturecounter/CustomNativeButtonManager.java
@@ -0,0 +1,71 @@
+package com.oldarchitecturecounter;
+
+import android.view.View;
+import android.widget.Button;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatButton;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.common.MapBuilder;
+import com.facebook.react.uimanager.SimpleViewManager;
+import com.facebook.react.uimanager.ThemedReactContext;
+import com.facebook.react.uimanager.annotations.ReactProp;
+import com.facebook.react.uimanager.events.RCTEventEmitter;
+
+import java.util.Map;
+
+public class CustomNativeButtonManager extends SimpleViewManager<AppCompatButton> {
+
+    public static final String REACT_CLASS = "CustomNativeButton";
+    ReactApplicationContext mCallerContext;
+
+    public CustomNativeButtonManager(ReactApplicationContext reactContext) {
+        mCallerContext = reactContext;
+    }
+
+    @NonNull
+    @Override
+    public String getName() {
+        return REACT_CLASS;
+    }
+
+    @NonNull
+    @Override
+    public AppCompatButton createViewInstance(ThemedReactContext context) {
+        AppCompatButton button = new AppCompatButton(context);
+        onReceiveNativeEvent(context, button);
+        return button;
+    }
+
+    @ReactProp(name = "title")
+    public void setTitle(AppCompatButton button, @Nullable String title) {
+        if (title != null) {
+            button.setText(title);
+        }
+        else {
+            button.setText("Default Button");
+        }
+    }
+
+
+    public void onReceiveNativeEvent(ThemedReactContext context, Button button) {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(@NonNull View b) {
+                WritableMap event = Arguments.createMap();
+                context.getJSModule(RCTEventEmitter.class).receiveEvent(button.getId(), "onClick", event);
+            }
+        });
+    }
+
+
+    @Nullable @Override
+    public Map<String, Object> getExportedCustomDirectEventTypeConstants() {
+        return MapBuilder.<String, Object>builder()
+                .put("onClick",
+                        MapBuilder.of("registrationName", "onClick"))
+                .build();
+    }
+}
